cd741a4780d017d4a7faf2b52b914e5c3f7c2e26
無職やめ太郎（本名） <jazzm@ster.jp.net>
2025-09-27 10:55:27 +0900

    Update problem-flow.tsx

--- src/components/problem/problem-flow.tsx
+++ src/components/problem/problem-flow.tsx
@@ -814,7 +814,7 @@ export default function ProblemFlow({ length }: ProblemFlowProps) {
             </h2>
             {isCorrect && (
               <>
-                <p className="mt-4 text-lg font-semibold text-[#2a2b3c]">
+                <p className="mt-4 text-xl font-semibold text-[#2a2b3c]">
                   {problem.englishSentence}
                 </p>
                 <p className="mt-4 text-lg font-semibold text-[#2a2b3c]">
