1f99af1447347875732c819de59c272b66760fd1
徳本 嵩 <t_tokumoto@yumemi.co.jp>
2025-10-04 05:00:51 +0900

    no message

--- src/app/problems/[type]/page.tsx
+++ src/app/problems/[type]/page.tsx
@@ -47,7 +47,7 @@ function LoadingFallback() {
   return (
     <div className="relative max-w-[500px] mx-auto">
       {/* 画像と同じアスペクト比（500x750 = 2:3）のプレースホルダー */}
-      <div className="w-full aspect-[2/3] bg-[#f4f1ea]/30 rounded-lg" />
+      <div className="w-full aspect-[2/3] rounded-lg" />
 
       <div className="absolute inset-0 flex items-center justify-center">
         <StartButton error={null} disabled>
