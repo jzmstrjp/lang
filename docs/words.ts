d635a92c31e6ac3296b8e3fc60aa1bb4694a6899
takashi.tokumoto <t_tokumoto@yumemi.co.jp>
2026-01-07 18:08:21 +0900

    no message

--- docs/words.ts
+++ docs/words.ts
@@ -1,5 +1,6 @@
-export const words: `${string}|【${"ビジネス"|"私生活"}系】${"対面"|"電話" | "ビデオ通話"}で${string}いる場面`[] = [
-  'as soon as possible|【ビジネス系】対面で見積もり依頼をしている場面',
-  'according to|【ビジネス系】対面で説明をしている場面',
-  'make a reservation|【私生活系】電話でホテルの予約をしている場面',
-];
+export const words: `${string}|【${'ビジネス' | '私生活'}系】${'対面' | '電話' | 'ビデオ通話'}で${string}いる場面`[] =
+  [
+    'as soon as possible|【ビジネス系】対面で見積もり依頼をしている場面',
+    'according to|【ビジネス系】対面で説明をしている場面',
+    'make a reservation|【私生活系】電話でホテルの予約をしている場面',
+  ];
